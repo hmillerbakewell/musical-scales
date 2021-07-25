535233f3c08b97b68a6c5ea471c46b54b1ab6f70
Hector Miller-Bakewell <hmillerbakewell@gmail.com>
2021-07-25 10:41:35 +0100

    Changed autodoc source

--- build_docs.sh
+++ build_docs.sh
@@ -1,3 +1,3 @@
 rm -rf ./docs/build
-sphinx-apidoc -feo docs/source/ ./src/musical_scales
-sphinx-build -aEb html docs/source/ docs/build/
+sphinx-apidoc -o docs/source/ ./src/musical_scales
+sphinx-build -b html docs/source/ docs/build/
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/musical_scales'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 # -- Project information -----------------------------------------------------
